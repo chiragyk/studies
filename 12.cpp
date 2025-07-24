8a1278894f3f9713a246204b482b426ff0303b0c
Chirag Y K <151862545+chiragyk@users.noreply.github.com>
2025-07-24 14:25:20 +0530

    Add files via upload

---
+++ 12.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, area;
+
+    cout << "Enter the side of the square (a): ";
+    cin >> a;
+
+    area = a * a;
+
+    cout << "Area of the square (a^2) is: " << area << endl;
+
+    return 0;
+}
